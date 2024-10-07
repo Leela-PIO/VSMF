32ff3161306e232791371895759cf74f60b01b72
leela-pio <leela.kandimalla@programmers.io>
2024-10-07 16:01:14 +0530

    Test1

---
+++ CECUSER/CECUSER.COBOL(HELLO).cbl
@@ -0,0 +1,9 @@
+000100 IDENTIFICATION DIVISION.                                         00010000
+000200 PROGRAM-ID. HELLO.                                               00020000
+000300 ENVIRONMENT DIVISION.                                            00030000
+000400 PROCEDURE DIVISION.                                              00040000
+000500 1000-SECTION SECTION.                                            00050000
+000600 1000-MAIN.                                                       00060000
+000700      DISPLAY 'VS MAINFRAME TO GITHUB'                            00070000
+000800      DISPLAY 'PIO- Test5'.                                       00080003
+000900      STOP RUN.                                                   00090000
